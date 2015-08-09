b7e184995b07c4a38ecce014b2b7760669a1317a
wteng.wang <wteng.wang@qunar.com>
2015-08-09 17:17:41 +0800

    轮播图ok

--- js/beiyou.js
+++ js/beiyou.js
@@ -3,9 +3,18 @@ function init() {
 	$("#header_menu ul li a").hover(function() {
 		$(this).attr("class","current");	
 	},function() {
-		$(this)removeClass("current");
+		$(this).removeClass("current");
 	});
+
+	$('#one').ContentSlider({
+                    width : '920px',
+                    height : '200px',
+                    speed : 800,
+                    easing : 'easeInOutBack'
+              });
 }
+
+
 
 
 $(document).ready(function () {
